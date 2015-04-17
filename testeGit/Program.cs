ef679ff603a637ec580019aa654428d5c723c321
MonDamon <clsmanfredine@hotmail.com>
2015-04-17 15:09:24 -0300

    teste4

--- testeGit/Program.cs
+++ testeGit/Program.cs
@@ -10,8 +10,8 @@ namespace testeGit
     {
         static void Main(string[] args)
         {
-            Console.Write("Teste Git 2");
-            Console.ReadKey();
+            Console.Write("Teste Git");
+            Console.ReadKey(); 
         }
     }
 }
